c5fffe48efdb548011b5e1818d018c1d1b51d9e9
theodorepgl <limlimtheo96@gmail.com>
2023-11-03 11:16:05 +0800

    Review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
          
   has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   enum :role, { customer: 0, admin: 1 }
 
