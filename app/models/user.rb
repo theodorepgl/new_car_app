5227f8752bee12242dfc5afeab27f7ae56e4c731
Theo <limlimtheo96@gmail.com>
2023-11-08 17:38:31 +0800

    USER

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable
          
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
