67011a1d7dfd226256655bcc6f79e1ac7f5cf0a2
Theo <limlimtheo96@gmail.com>
2023-11-09 10:43:58 +0800

    ABOUT

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'about', to: 'home#about'
   # Defines the root path route ("/")
   root "listings#index"
   resources :listings
