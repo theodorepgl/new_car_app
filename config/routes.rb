621c989cf8386e26353fb79ec4f81401d6770f11
Theo <limlimtheo96@gmail.com>
2023-11-09 12:42:32 +0800

    Kudat

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get 'about', to: 'home#about'
+  get 'kudat', to: 'home#kudat'
   # Defines the root path route ("/")
   root "listings#index"
   resources :listings
