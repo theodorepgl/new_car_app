f5225a2f36cefd47577bca8b61cd4c95905da51c
Theo <limlimtheo96@gmail.com>
2023-11-09 10:38:10 +0800

    ABOUT

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
 
 	def create
 		@review = Review.new(review_params)
-		@review.car_id = @booking.id 
+		@review.listing_id = @booking.id 
 		@review.user_id = current_user.id
 
 		if @review.save
